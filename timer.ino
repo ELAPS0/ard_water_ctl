79549b180451cf29154d8278f66567ff35efd330
bob <bob@elapso.work>
2017-06-03 13:05:17 +0200

    time settings done

--- timer.ino
+++ timer.ino
@@ -1,6 +1,8 @@
 #include <EEPROM.h>
 #include <Wire.h>
 #include <LiquidCrystal.h>
+#include "RTClib.h"
+RTC_DS1307 RTC;
 
 #define DS1307_I2C_ADDRESS 0x68
 
@@ -24,6 +26,7 @@ byte key(){ //// для кнопок ЛСДшилда
 }
 
 /////////// часы ..
+/*
 byte decToBcd(byte val){
   return ( (val/10*16) + (val%10) );
 }
@@ -74,12 +77,17 @@ void getDateDs1307(byte *second,
   *dayOfMonth = bcdToDec(Wire.read());
   *month      = bcdToDec(Wire.read());
   *year       = bcdToDec(Wire.read());
-}
+}*/
+
 ////
 void setClock(){ // установка часов
   byte pos = 1;
-  byte second, minute, hour, dayOfWeek, dayOfMonth, month, year; 
-    getDateDs1307(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+  char s0[16];
+  
+  //byte second, minute, hour, dayOfWeek, dayOfMonth, month, year; 
+  //getDateDs1307(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+
+    DateTime now = RTC.now(); 
   
     lcd.clear();
     lcd.blink();
@@ -87,53 +95,37 @@ void setClock(){ // установка часов
    while(key() != 1){ // крутим   цикл   
     byte KEY = key(); // читаем состояние кнопок
       delay(200);
+
+      
     lcd.setCursor(1, 1);
     lcd.print("set to save");
-    lcd.setCursor(0, 0);     // выводим инфу
-     if (hour < 10) lcd.print("0");
-    lcd.print(hour);
-    lcd.print(":");
-     if (minute < 10) lcd.print("0"); 
-    lcd.print(minute);  
-    lcd.print(" ");     
-     if (dayOfMonth < 10) lcd.print("0");
-    lcd.print(dayOfMonth);
-    lcd.print("/");
-     if (month < 10) lcd.print("0");
-    lcd.print(month);
-    lcd.print("/");
-     if (year < 10) lcd.print("0");
-    lcd.print(year);
+    lcd.setCursor(0, 0);  
+    sprintf (s0, "%02i:%02i %02i/%02i/%02i", now.hour(), now.minute(), now.day(), now.month(), now.year2());
+    lcd.print(s0);
     
     lcd.setCursor(pos, 0); // устанавливаем курсор согласно позиции
     
     if (KEY == 5 && pos < 13) pos += 3; // крутим позицию
     else if (KEY == 2 && pos > 1) pos -= 3;
     
-    else if (pos == 1 && KEY == 3) hour++; // крутим значения
-    else if (pos == 1 && KEY == 4) hour--;
-    else if (pos == 4 && KEY == 3) minute++;
-    else if (pos == 4 && KEY == 4) minute--;    
-    else if (pos == 7 && KEY == 3) dayOfMonth++;
-    else if (pos == 7 && KEY == 4) dayOfMonth--;    
-    else if (pos == 10 && KEY == 3) month++;
-    else if (pos == 10 && KEY == 4) month--;    
-    else if (pos == 13 && KEY == 3) year++;
-    else if (pos == 13 && KEY == 4) year--;  
-    
-    if (hour > 23) hour = 0;
-    else if (minute > 59) minute = 0;
-    else if (dayOfMonth > 31) dayOfMonth = 0;
-    else if (month > 12) month = 1;
-    else if (year > 99) year = 0;
-  }// конец цикла
+    else if (pos == 1 && KEY == 3) now.inc_hour(); // крутим значения
+    else if (pos == 1 && KEY == 4) now.dec_hour();
+    else if (pos == 4 && KEY == 3) now.inc_minute();
+    else if (pos == 4 && KEY == 4) now.dec_minute();    
+    else if (pos == 7 && KEY == 3) now.inc_day();
+    else if (pos == 7 && KEY == 4) now.dec_day();    
+    else if (pos == 10 && KEY == 3) now.inc_month();
+    else if (pos == 10 && KEY == 4) now.dec_month();    
+    else if (pos == 13 && KEY == 3) now.inc_year2();
+    else if (pos == 13 && KEY == 4) now.dec_year2();  
+  }
   
- setDateDs1307(second, minute, hour, dayOfWeek, dayOfMonth, month, year); 
-   lcd.noBlink(); 
-   lcd.clear();
-   lcd.print("     Saved");
-   delay(1500);
-}///
+  RTC.adjust(now);
+  lcd.noBlink(); 
+  lcd.clear();
+  lcd.print("     Saved");
+  delay(1500);
+}
 
 void setOnOff(){    
   byte pos = 0;   
@@ -214,6 +206,11 @@ void menu(){
 }  
  
 void setup(){
+  //отладка 
+  Serial.begin(9600);
+  
+  RTC.begin();
+  
   Wire.begin(); 
   lcd.begin(16, 2);
   lcd.clear();
@@ -225,64 +222,58 @@ void setup(){
   setHorClockOn = EEPROM.read(1);
   setMinClockOff = EEPROM.read(3);
   setHorClockOff = EEPROM.read(4);
+  
+  if (! RTC.isrunning()) {
+    RTC.adjust(DateTime(__DATE__, __TIME__));
+Serial.println("RTC is NOT running. Set scketch compilling time");
+  }
+  
+
+  
 }
 
 void loop()
 {
-  byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;  
-  getDateDs1307(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+  int state = digitalRead(outPin);
+  char s0[16];
+  
+  //byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;  
+  //getDateDs1307(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+
+  DateTime now = RTC.now();
   
   // обработка кнопок
   if (key() == 1) menu(); // если нажата селект
   else if (key() == 3) digitalWrite(outPin, HIGH);
   else if (key() == 4) digitalWrite(outPin, LOW);
 
+
   // сравниваем время и управляем выходом// 
-  if (setMinClockOff == minute && setHorClockOff == hour
-      && second == 0) digitalWrite(outPin, LOW);
-  if (setMinClockOn == minute && setHorClockOn == hour
-      && second == 0) digitalWrite(outPin, HIGH);    
- 
- 
-//   lcd.clear();
-    lcd.setCursor(0, 0);
-     if (hour < 10) lcd.print("0"); 
-    lcd.print(hour); 
-    lcd.print(":");
-     if (minute < 10) lcd.print("0"); 
-    lcd.print(minute);
-//  lcd.print(":");
-//   if (second < 10) lcd.print("0");
-//  lcd.print(second);
-//    lcd.setCursor(8, 0); 
-//    lcd.print("    ");  
-    lcd.setCursor(0, 1);
-     if (dayOfMonth < 10) lcd.print("0");
-    lcd.print(dayOfMonth);
-    lcd.print("/");
-     if (month < 10) lcd.print("0");
-    lcd.print(month);
-    lcd.print("/");
-     if (year < 10) lcd.print("0");
-    lcd.print(year);
-     //
-    lcd.setCursor(11, 0);
-     if (setHorClockOn < 10) lcd.print("0"); 
-    lcd.print(setHorClockOn); 
-    lcd.print(":");
-     if (setMinClockOn < 10) lcd.print("0"); 
-    lcd.print(setMinClockOn);
-    
-    lcd.setCursor(11, 1);
-     if (setHorClockOff < 10) lcd.print("0"); 
-    lcd.print(setHorClockOff); 
-    lcd.print(":");
-     if (setMinClockOff < 10) lcd.print("0"); 
-    lcd.print(setMinClockOff);      
-   
-    lcd.setCursor(7, 0);
-    if (digitalRead(outPin)) lcd.print("ON ");
-    else lcd.print("Off");
-    
-    delay(200); // нужно для нармальной работы кнопок
+  if (setMinClockOff == now.minute() && setHorClockOff == now.hour()){
+    if ( state != LOW)
+    {
+      digitalWrite(outPin, LOW);
+      Serial.println("Out pin down");
+    }
+  }
+  
+  if (setMinClockOn == now.minute() && setHorClockOn == now.hour()){
+    if ( state == LOW)
+    {
+      digitalWrite(outPin, HIGH);
+      Serial.println("Out pin up");
+    }
+  }
+
+
+  
+  sprintf (s0, "%02i:%02i:%02i %c %02i:%02i", now.hour(), now.minute(), now.second(), state==LOW?'v':'^', setMinClockOn, setHorClockOn);
+  lcd.setCursor(0,0);
+  lcd.print(s0);
+
+  sprintf (s0, "%02i/%02i/%02i   %02i:%02i", now.day(), now.month(), now.year2(), setMinClockOff, setHorClockOff);
+  lcd.setCursor(0,1);
+  lcd.print(s0);
+  
+  delay(200); // нужно для нармальной работы кнопок
 }
